04f726496e0e501fb44a6826090629e920d708ed
Mayur Susare <mayursusare@winjit.com>
2021-05-09 16:10:08 +0530

    Initial Commit

---
+++ MVVMArchitecturePattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMArchitecturePattern
+//
+//  Created by Mayur Susare on 09/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
